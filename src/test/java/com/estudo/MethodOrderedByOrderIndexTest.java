85f97066af5b730c39af9e82c878f949ef26083e
Geovanni dos Santos Correia <geovanni-dsc@hotmail.com>
2023-09-17 18:11:40 -0300

    Feat: Ciclo de Vida do JUnit 5, com @TestInstance

--- src/test/java/com/estudo/MethodOrderedByOrderIndexTest.java
+++ src/test/java/com/estudo/MethodOrderedByOrderIndexTest.java
@@ -1,39 +1,50 @@
 package com.estudo;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 
-//@TestMethodOrder(MethodOrderer.MethodName.class)
-//@TestMethodOrder(MethodOrderer.Random.class)
+@TestInstance(Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class MethodOrderedRandonlyTest {
+class MethodOrderedByOrderIndexTest {
 
+StringBuilder actualValue = new StringBuilder("");
 	
+	@AfterEach
+	void afterEach() {
+		System.out.println("The actual value is: "+ actualValue);
+	}
 	
 	@Test
 	@Order(1)
 	void testA() {
 		System.out.println("Running Test A");
+		actualValue.append("1");
 	}
 	
 	@Test
 	@Order(3)
 	void testB() {
 		System.out.println("Running Test B");
+		actualValue.append("2");
 	}
 	
 	@Test
 	@Order(2)
 	void testC() {
 		System.out.println("Running Test C");
+		actualValue.append("3");
 	}
 	
 	@Test
 	@Order(4)
 	void testD() {
 		System.out.println("Running Test D");
+		actualValue.append("4");
 	}
 
 }
